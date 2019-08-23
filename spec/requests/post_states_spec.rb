bf32b7a15ec16b49a1ef0a9915d193c67cd2c937
Lake Bramlett <lakebramlett@Lakes-MBP.hsd1.or.comcast.net>
2019-08-23 13:27:11 -0700

    add State creation request spec

--- spec/requests/post_states_spec.rb
+++ spec/requests/post_states_spec.rb
@@ -6,6 +6,10 @@ describe "post a state route", :type => :request do
     post '/v1/states', params: { :name => 'New State'}
   end
 
+  it 'creates a new state' do
+    expect(State.count).to eq(1)
+  end
+
   it 'returns a creates status' do
     expect(response).to have_http_status(:created)
   end
