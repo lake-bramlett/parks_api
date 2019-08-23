b46c24d5b1eaaca480168c759b6447561201bf5a
Lake Bramlett <lakebramlett@Lakes-MBP.hsd1.or.comcast.net>
2019-08-23 08:46:00 -0700

    generate schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :states do
-    reources :parks
+    resources :parks
   end
 end
---
+++ db/migrate/20190823153901_add_parks_table.rb
@@ -0,0 +1,11 @@
+class AddParksTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :parks do |t|
+      t.string :name
+      t.string :park_ranger
+      t.boolean :open
+
+      t.belongs_to :park, foreign_key: true
+    end
+  end
+end
