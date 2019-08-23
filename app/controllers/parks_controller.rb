f6f060e53f73686e3b1c176a57e0c0389072912f
Lake Bramlett <lakebramlett@Lakes-MBP.hsd1.or.comcast.net>
2019-08-23 09:33:36 -0700

    add controllers

---
+++ app/controllers/parks_controller.rb
@@ -0,0 +1,49 @@
+class ParksController < ApplicationController
+  def index
+    @state = State.find(params[:state_id])
+    @parks = @state.parks.all
+    json_response(@parks)
+  end
+
+  def show
+    @state = State.find(params[:state_id])
+    @park = Park.find(params[:id])
+    json_response(@park)
+  end
+
+  def create
+    @state = State.find(params[:state_id])
+    @park = @state.parks.create!(park_params)
+    json_response(@park)
+    render status: 200, json: {
+     message: "This park has been created."
+     }
+  end
+
+  def update
+    @state = State.find(params[:state_id])
+    @park = Park.find(params[:id])
+    @state.parks.update(park_params)
+    render status: 200, json: {
+     message: "This park has been updated successfully."
+     }
+  end
+
+  def destroy
+    @state = State.find(params[:state_id])
+    @park = Park.find(params[:id])
+    @park.destroy
+    render status: 200, json: {
+     message: "This park has been destroyed."
+     }
+  end
+
+  private
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def park_params
+    params.permit(:name, :park_ranger, :open)
+  end
+end
---
+++ app/controllers/states_controller.rb
@@ -0,0 +1,35 @@
+class StatesController < ApplicationController
+  def index
+    @states = State.all
+    json_response(@states)
+  end
+
+  def show
+    @state = State.find(params[:id])
+    json_response(@state)
+  end
+
+  def create
+    @state = State.create(state_params)
+    json_response(@state)
+  end
+
+  def update
+    @state = State.find(params[:id])
+    @state.update(state_params)
+  end
+
+  def destroy
+    @state = State.find(params[:id])
+    @state.destroy
+  end
+
+  private
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def state_params
+    params.permit(:name)
+  end
+end
