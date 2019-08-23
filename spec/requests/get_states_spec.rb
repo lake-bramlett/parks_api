90ee089d54815cd1a4f3847ae7d5fdb9b9f56fd1
Lake Bramlett <lakebramlett@Lakes-MBP.hsd1.or.comcast.net>
2019-08-23 09:43:54 -0700

    add initial request spec

---
+++ spec/requests/get_states_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "get all quotes route", :type => :request do
+  let!(:states) { FactoryBot.create_list(:state, 20)}
+
+  before { get '/states'}
+
+  it 'returns all states' do
+    expect(JSON.parse(response.body).size).to eq(20)
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
